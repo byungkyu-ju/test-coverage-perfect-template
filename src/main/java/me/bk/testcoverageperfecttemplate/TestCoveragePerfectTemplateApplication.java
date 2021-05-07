f84b822b72ea6500a241e9e264ce5ef428dce6b8
byungkyu-ju <noel1227@gmail.com>
2021-05-07 19:15:29 +0900

    Initial commit

---
+++ src/main/java/me/bk/testcoverageperfecttemplate/TestCoveragePerfectTemplateApplication.java
@@ -0,0 +1,13 @@
+package me.bk.testcoverageperfecttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCoveragePerfectTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCoveragePerfectTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/bk/testcoverageperfecttemplate/TestCoveragePerfectTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bk.testcoverageperfecttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCoveragePerfectTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
